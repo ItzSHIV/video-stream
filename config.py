8cb3edb3a5c69434f898fa730113e3d4ae9aa30c
shiv munot <shivmunot971@gmail.com>
2021-12-09 13:43:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,3 +26,4 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/levina-lab/video-str
 IMG_1 = getenv("IMG_1", "https://telegra.ph/file/ab008e2712123d0bf5209.jpg")
 IMG_2 = getenv("IMG_2", "https://telegra.ph/file/a2f9ef0a51907c7a278c9.jpg")
 IMG_3 = getenv("IMG_3", "https://telegra.ph/file/5301814e2e4d97afb7ca7.jpg")
+IMG_4 = getenv("IMG_4", "https://telegra.ph/file/ec8a5cf6ae48516bc352d.jpg")
